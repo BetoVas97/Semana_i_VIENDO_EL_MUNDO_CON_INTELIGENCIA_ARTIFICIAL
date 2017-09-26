e836d720b39a6303317238d3b62eafd688141bf9
BetoVas97 <32272824+BetoVas97@users.noreply.github.com>
2017-09-26 12:41:03 -0500

    Subiendo MS IA 1.0

---
+++ MS IA/Assets/cubeCamera.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class cubeCamera : MonoBehaviour {
+
+    WebCamTexture _webcamtex;
+    public int imagesCounter;
+	// Use this for initialization
+	void Start () {
+        imagesCounter = PlayerPrefs.GetInt("imgCounter");
+		_webcamtex = new WebCamTexture();
+		Renderer _renderer = GetComponent<Renderer>();
+		_renderer.material.mainTexture = _webcamtex;
+		_webcamtex.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.A))
+		{
+			StartCoroutine(CaptureTextureAsPNG());
+		}
+	}
+	IEnumerator CaptureTextureAsPNG()
+	{
+		yield return new WaitForEndOfFrame();
+		Texture2D _TextureFromCamera = new Texture2D(GetComponent<Renderer>().material.mainTexture.width,
+		GetComponent<Renderer>().material.mainTexture.height);
+		_TextureFromCamera.SetPixels((GetComponent<Renderer>().material.mainTexture as WebCamTexture).GetPixels());
+		_TextureFromCamera.Apply();
+		byte[] bytes = _TextureFromCamera.EncodeToPNG();
+        string filePath = Application.dataPath + "/Assets/SavedScreen," + imagesCounter + ".png";
+		//+ "screenshots/" +
+		File.WriteAllBytes(filePath, bytes);
+
+	}
+	
+}
