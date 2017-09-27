c49b335383f2ef5cc57e32ad7567aa6a48fdd145
Irvin Trujillo <irving_emmanuel95@hotmail.com>
2017-09-27 00:22:15 -0500

    Se agrego lectura de JSON y envio de JSON al algoritmo dde detecccion de desastres, la deteccion de desastres se mejoró un poco

--- MS IA/Assets/Scripts/ImageToComputerVisionAPI.cs
+++ MS IA/Assets/Scripts/ImageToComputerVisionAPI.cs
@@ -7,16 +7,14 @@ using System.Collections.Generic;
 
 public class ImageToComputerVisionAPI : MonoBehaviour {
 
-    string VISIONKEY = "5e72ba714a39441d962d94f43481112f"; // replace with your Computer Vision API Key
-
-    string emotionURL = "https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/analyze";
-
+    string VISIONKEY = "5e72ba714a39441d962d94f43481112f"; // Computer Vision API Key
+    string emotionURL = "https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/analyze?visualFeatures=Categories,Description,Color&language=en"; // despues del "?" son los atributos que se quieren consultar de la imagen :v
     public string fileName { get; private set; }
     string responseData;
 
     // Use this for initialization
     void Start () {
-	    fileName = Path.Combine(Application.streamingAssetsPath, "cityphoto.jpg"); // Replace with your file
+        fileName = Path.Combine(Application.streamingAssetsPath, "incendio.jpg"); // Replace with your file
     }
 	
 	// Update is called once per frame
@@ -44,9 +42,18 @@ public class ImageToComputerVisionAPI : MonoBehaviour {
         };
 
         WWW www = new WWW(emotionURL, bytes, headers);
-
         yield return www;
+
+        
         responseData = www.text; // Save the response as JSON string
-        GetComponent<ParseComputerVisionResponse>().ParseJSONData(responseData);
+        //print("Respuesta de la solicitud: " + responseData);
+
+      
+        JSONObject dataArray = new JSONObject(responseData);
+        print("Imprimiendo objeto JSON: " + dataArray.ToString()); //json ejemplo: "categories":[{"name":"dark_fire","score":0.90625}],"description":{"tags":["smoke","train","coming","track","grass","steam","air","country","clouds","old","forest","fire","dark","stop","mountain","throwing","sun","riding","intersection","water"],"captions":[{"text":"a train on a track with smoke coming out of it","confidence":0.2727745}]},"requestId":"79ebe576-ffe1-4518-96de-07f03b83138a","metadata":{"width":1280,"height":720,"format":"Jpeg"},"color":{"dominantColorForeground":"Brown","dominantColorBackground":"Brown","dominantColors":["Brown","Orange"],"accentColor":"C79304","isBWImg":false}}
+        print("Respuesta EN IMAGETOCOMPUTER-tags: :v " + dataArray.list[1].list[0]);//imprimiendo lista de tags, ya que la objtengo del json
+        AlgoritmoDeDeteccionDeDesastres lol = new AlgoritmoDeDeteccionDeDesastres(dataArray.list[1].list[0]); //creo la clase mandandole el jsonobject :v- INTERNAMEMTE EN ESTA CLASE AL HACER EL START SE MANDAN A LLAMAR LAS FUNCIONES CORRESPONDIENTES
+        //GetComponent<ParseComputerVisionResponse>().ParseJSONData(responseData);  //se comenta ya que no lo usaremos en el sistema al momento .. :v
     }
+
 }
